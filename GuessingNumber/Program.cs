cb7a64038d3e8b8edd09876dbfa926bbf9dbcc6f
lazarus43 <nhingoc204@gmail.com>
2022-01-17 19:28:43 +0700

    update

--- GuessingNumber/Program.cs
+++ GuessingNumber/Program.cs
@@ -1,43 +1,43 @@
 ﻿
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, It's Guessing Game!");
 
 
 Console.WriteLine(" ");
 
 Random rNum = new Random();
 
-int wNum = rNum.Next(0,100);
-
-int iNum;
+var wNum = rNum.Next(0,100);
 
 bool wStatus = false;
 
 while (wStatus == false)
 {
-    //Console.WriteLine("Enter a number: ");
-    //iNum = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Enter a number : ");
-    while (!int.TryParse(Console.ReadLine(), out iNum))
+    Console.WriteLine("Enter a number: ");
+    //var iNum = Convert.ToInt32(Console.ReadLine());   
+    if (int.TryParse(Console.ReadLine(), out var iNum))
     {
-        Console.Write("This is not valid input. Please enter an integer value: ");
-    }
-    Console.Clear();
-    if (iNum != wNum)
-    {
-        Console.WriteLine("Wrong number try again!!!!");
-    }
-    if(iNum > wNum)
-    {
-        Console.WriteLine("Try lower");
-    }
-    else if(iNum < wNum)
-    {
-        Console.WriteLine("Try higher");
+        Console.Clear();
+        if (iNum != wNum)
+        {
+            Console.WriteLine("Wrong number try again!!!!");
+        }
+        if (iNum > wNum)
+        {
+            Console.WriteLine("Try lower");
+        }
+        else if (iNum < wNum)
+        {
+            Console.WriteLine("Try higher");
+        }
+        else
+        {
+            Console.WriteLine("                  Congrats You Win!!!            ");
+            wStatus = true;
+        }
     }
     else
     {
-        Console.WriteLine("                  Congrats You Win!!!            ");
-        wStatus = true;
+        Console.WriteLine("Not valid number");
     }
 }
 Console.WriteLine("Press Any key to Finnish");
