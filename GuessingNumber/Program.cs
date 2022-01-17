3c0950ef5b6281e729a395bb5d6bedc3a5ab273e
lazarus43 <nhingoc204@gmail.com>
2022-01-17 18:01:33 +0700

    Starter

---
+++ GuessingNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(" ");
+
+Random rNum = new Random();
+
+int wNum = rNum.Next(0,100);
+
+int iNum;
+
+bool wStatus = false;
+
+while (wStatus == false)
+{
+    //Console.WriteLine("Enter a number: ");
+    //iNum = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter a number : ");
+    while (!int.TryParse(Console.ReadLine(), out iNum))
+    {
+        Console.Write("This is not valid input. Please enter an integer value: ");
+    }
+    Console.Clear();
+    if (iNum != wNum)
+    {
+        Console.WriteLine("Wrong number try again!!!!");
+    }
+    if(iNum > wNum)
+    {
+        Console.WriteLine("Try lower");
+    }
+    else if(iNum < wNum)
+    {
+        Console.WriteLine("Try higher");
+    }
+    else
+    {
+        Console.WriteLine("                  Congrats You Win!!!            ");
+        wStatus = true;
+    }
+}
+Console.WriteLine("Press Any key to Finnish");
+Console.ReadKey();
